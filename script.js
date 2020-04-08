f4377a0fa914381e434d9050fa2209d8387406af
forough1370 <61025528+forough1370@users.noreply.github.com>
2020-04-08 14:06:18 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -3,6 +3,35 @@
     $(".toggleText").toggle();
   });
 });*/
-function toggleImage() {
-  $(".hiddenclickimg").toggle();
-};
+// function toggleImage() {
+//   $(".hiddenclickimg").toggle();
+// };
+
+function mouseIn() {
+  $('.img').addClass('show');
+}
+
+function mouseOut() {
+  $('.img').removeClass('show');
+}
+
+$('.hover-me').hover(mouseIn, mouseOut);
+
+$(document).ready(function() {
+  var bodyWidth = document.body.clientWidth
+  var bodyHeight = document.body.clientHeight;
+
+$('.img').each(function(idx, img) {
+    var randPosX = Math.floor((Math.random() * bodyWidth));
+    var randPosY = Math.floor((Math.random() * bodyHeight));
+    console.log(randPosY);
+    $(img).css('left', randPosX);
+    $(img).css('top', randPosY);
+
+  });
+});
+
+$('.next-one').click(function(){
+    $('.box-two').show();
+    $('.box-two').hide();
+ });
